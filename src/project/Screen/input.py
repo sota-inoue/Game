62509647fba1ac652233a3fc37654d0a1ff88bc2
kurage-330 <linxiaocou80@gmail.com>
2026-06-24 20:26:08 +0900

    コード修正

--- src/project/Screen/input.py
+++ src/project/Screen/input.py
@@ -1,7 +1,8 @@
 from evdev import InputDevice, ecodes
 
+
 class TouchInput:
-    
+
     DEVICE = "/dev/input/event11"
 
     def __init__(self):
@@ -12,25 +13,21 @@ class TouchInput:
         self.touch_down = False
 
     def update(self):
-        self.touch_down = False
-
         try:
-            events = self.touch.read()
+            for event in self.touch.read():
+
+                if event.type == ecodes.EV_ABS:
+
+                    if event.code == ecodes.ABS_X:
+                        self.x = event.value
+
+                    elif event.code == ecodes.ABS_Y:
+                        self.y = event.value
+
+                elif event.type == ecodes.EV_KEY:
+
+                    if event.code == ecodes.BTN_TOUCH:
+                        self.touch_down = bool(event.value)
 
         except BlockingIOError:
             return
-
-        for event in events:
-
-            if event.type == ecodes.EV_ABS:
-
-                if event.code == ecodes.ABS_X:
-                    self.x = event.value
-
-                elif event.code == ecodes.ABS_Y:
-                    self.y = event.value
-
-            elif event.type == ecodes.EV_KEY:
-
-                if event.code == ecodes.BTN_TOUCH:
-                    self.touch_down = bool(event.value)
