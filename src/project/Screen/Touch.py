cb52210c15a54e8da350c85da6a8104dfd481de0
kurage-330 <linxiaocou80@gmail.com>
2026-06-24 19:04:22 +0900

    コード修正

--- src/project/Screen/Touch.py
+++ src/project/Screen/Touch.py
@@ -6,10 +6,10 @@ class TouchDisplay:
     HEIGHT = 320
 
     def __init__(self):
-        self.surface = pygame.Surface((self.WIDTH, self.HEIGHT))
+        self.surface = pygame.Surface((self.WIDTH, self.HEIGHT), depth=16)
 
     def draw(self):
         self.surface.fill((0, 0, 0))
 
-        pygame.draw.rect( self.surface, (255, 0, 0), (50, 50, 150, 100) )
+        #pygame.draw.rect( self.surface, (255, 0, 0), (50, 50, 150, 100) )
         pygame.draw.circle(self.surface, (0, 255, 0), (300, 160), 50)
--- src/project/Screen/fb.py
+++ src/project/Screen/fb.py
@@ -7,29 +7,16 @@ class FrameBuffer:
         self.device = device
         self.width = width
         self.height = height
+        self.size = width * height * 2
 
         self.file = open(self.device, "r+b")
-        self.map = mmap.mmap(
-            self.file.fileno(),
-            self.width * self.height * 2
-        )
+        self.map = mmap.mmap(self.file.fileno(), self.size)
 
     def draw(self, surface):
-        raw = pygame.image.tostring(surface, "RGB")
-
-        data = bytearray()
-
-        for i in range(0, len(raw), 3):
-            r = raw[i]
-            g = raw[i + 1]
-            b = raw[i + 2]
-
-            rgb565 = ((r & 0xF8) << 8) | ((g & 0xFC) << 3) | (b >> 3)
-
-            data += rgb565.to_bytes(2, "little")
+        raw = pygame.image.tostring(surface, "RGB565")
 
         self.map.seek(0)
-        self.map.write(data)
+        self.map.write(raw)
 
     def close(self):
         self.map.close()
