22b8a0ad32811c6716f397ccaa721e77d621646c
inosou <kiribachi.12@gmail.com>
2026-09-04 18:31:11 +0900

    fix: Enemy.pyおよびObstacle.pyのファイル再生成とインポート不整合の修正

--- Src/Project/Domain/object_parameters.py
+++ Src/Project/Domain/object_parameters.py
@@ -9,3 +9,13 @@ OJISAN_IS_JUMPABLE = False
 BANANA_ID = 51
 BANANA_DAMAGE  = 10
 BANANA_IS_JUMPABLE = True
+
+# カラーコーン
+COLOR_CONE_DAMAGE = 10
+COLOR_CONE_IS_JUMPABLE = False
+
+# ２マスのキャラ
+
+# ２マスの障害物（キャリーケース）
+
+# 一般人
